6f0faa8ffbb395e003daee75dad674f83533a5bc
ShortyTapki <v.v.vinogradov99@gmail.com>
2022-11-15 17:21:03 +0300

    Updated api

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const getPost = async (id) => {
   return await postRes.json();
 };
 
-app.get('/api/kids/:id', cors(), async (req, res) => {
+// Get post's main comments
+
+app.get('/api/roots/:id', cors(), async (req, res) => {
   const itemId = req.params.id;
   const itemData = await getPost(itemId);
   const roots = itemData?.kids;
@@ -35,22 +37,13 @@ app.get('/api/kids/:id', cors(), async (req, res) => {
   res.json(comments);
 });
 
-// [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ]
+app.get('/api/:item', cors(), async (req, res) => {
+  res.json(await getPost(req.params.item));
+});
 
-// app.get('/api/kids/:id', cors(), async (req, res) => {
-//   const { kids: kidsIds } = await getPost(req.params.parent);
-//   let replies = [];
-//   if (kids) {
-//     for (let id = 0; id < kidsIds.length; id++)
-//       replies.push(await getPost(kidsIds[id]));
-//   } else {
-//     replies.push(null);
-//   }
-//   res.json(replies);
-// });
-
+// Initial point
 app.get('/api/latest', cors(), async (req, res) => {
-  const AMOUNT_OF_POSTS = 10;
+  const AMOUNT_OF_POSTS = 20;
 
   const srotiesRes = await fetch(
     'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty'
